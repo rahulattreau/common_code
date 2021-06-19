239a3f4341276c555bc3393ad8cfec82da73ef3a
Rahul <rahul@treau.cool>
2021-06-18 20:09:35 -0700

    1. Works. 2. But found error, the if statement is mistakenly in the for loop. Need to remove that!

--- lookup_table/lookup_table.c
+++ lookup_table/lookup_table.c
@@ -10,6 +10,7 @@ float LookupTableFunc(float const *breakpoints, float const *table_values, const
     // outside_range stores that the value was outside the x-range of the lookup-table data so that 
     // extrapolation can be performed if required
     bool outside_range = true;
+    int kFirstBreakpointIndex = 0;
 
     // cycle through array to see if x_value lies between breakpoint entries
     for(int j = 0; j < (length - 1); j++) {
@@ -17,12 +18,17 @@ float LookupTableFunc(float const *breakpoints, float const *table_values, const
         // check if x_value is between consecutive breakpoints
         if ( ( *(breakpoints + j) <= x_value  ) && ( x_value <= *(breakpoints + j + 1) ) ) {
             
-            y_value = LinearInterpolation(*(breakpoints + j), // x1
-                *(breakpoints + j + 1), // x2
-                *(table_values + j), // y1
-                *(table_values + j + 1), // y2
-                x_value // x
-                );
+            // define breakpoint indexes
+            kFirstBreakpointIndex = j;
+            // const int kSecondBreakpointIndex = kFirstBreakpointIndex + 1;
+            
+            // // linearly extrapolate below range
+            // y_value = LinearInterpolation(breakpoints[kFirstBreakpointIndex], // x1
+            //     breakpoints[kSecondBreakpointIndex], // x2
+            //     table_values[kFirstBreakpointIndex], // y1
+            //     table_values[kSecondBreakpointIndex], // y2
+            //     x_value // x
+            //     );
 
             // found that the value is in range
             outside_range = false;
@@ -35,16 +41,16 @@ float LookupTableFunc(float const *breakpoints, float const *table_values, const
             if(x_value < breakpoints[0]) { 
                 
                 // define breakpoint indexes
-                const int kFirstBreakpointIndex = 0;
-                const int kSecondBreakpointIndex = 1;
+                kFirstBreakpointIndex = 0;
+                // const int kSecondBreakpointIndex = 1;
                 
-                // linearly extrapolate below range
-                y_value = LinearInterpolation(breakpoints[kFirstBreakpointIndex], // x1
-                    breakpoints[kSecondBreakpointIndex], // x2
-                    table_values[kFirstBreakpointIndex], // y1
-                    table_values[kSecondBreakpointIndex], // y2
-                    x_value // x
-                    );
+                // // linearly extrapolate below range
+                // y_value = LinearInterpolation(breakpoints[kFirstBreakpointIndex], // x1
+                //     breakpoints[kSecondBreakpointIndex], // x2
+                //     table_values[kFirstBreakpointIndex], // y1
+                //     table_values[kSecondBreakpointIndex], // y2
+                //     x_value // x
+                //     );
 
             }
 
@@ -52,19 +58,31 @@ float LookupTableFunc(float const *breakpoints, float const *table_values, const
             else // if(x_value > breakpoints[length - 1])
             {
                 // define breakpoint indexes
-                const int kFirstBreakpointIndex = length - 2;
-                const int kSecondBreakpointIndex = kFirstBreakpointIndex + 1;
+                kFirstBreakpointIndex = length - 2;
+                // const int kSecondBreakpointIndex = kFirstBreakpointIndex + 1;
                 
-                // linearly extrapolate below range
-                y_value = LinearInterpolation(breakpoints[kFirstBreakpointIndex], // x1
-                    breakpoints[kSecondBreakpointIndex], // x2
-                    table_values[kFirstBreakpointIndex], // y1
-                    table_values[kSecondBreakpointIndex], // y2
-                    x_value // x
-                    );
+                // // linearly extrapolate below range
+                // y_value = LinearInterpolation(breakpoints[kFirstBreakpointIndex], // x1
+                //     breakpoints[kSecondBreakpointIndex], // x2
+                //     table_values[kFirstBreakpointIndex], // y1
+                //     table_values[kSecondBreakpointIndex], // y2
+                //     x_value // x
+                //     );
             }
         }
     }
 
+    // calculate the second breakpoint
+    const int kSecondBreakpointIndex = kFirstBreakpointIndex + 1;
+            
+    // linearly extrapolate below range
+    y_value = LinearInterpolation(breakpoints[kFirstBreakpointIndex], // x1
+        breakpoints[kSecondBreakpointIndex], // x2
+        table_values[kFirstBreakpointIndex], // y1
+        table_values[kSecondBreakpointIndex], // y2
+        x_value // x
+        );
+
     return y_value;
+
 }
