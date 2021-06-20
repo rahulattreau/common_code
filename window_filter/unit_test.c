ae316aa5fb137156558ce9d97f94abafa6cd9ae4
Rahul <rahul@treau.cool>
2021-06-20 16:54:05 -0700

    trimmed cruft. Removed StoreStateVariable function

--- window_filter/unit_test.c
+++ window_filter/unit_test.c
@@ -12,14 +12,14 @@ int main() {
     1.2, 
     3.3, 3.5, // out of range
     1.1, 0.9};
-    
+
     window_filter_t y;
     WindowFilterInit(&y, u_vector[0]);
 
     for (int j = 0; j < sizeof(u_vector) / sizeof(u_vector[0]); j ++) {
         WindowFilterFunc(&y, u_vector[j], 2.0);
         printf("u: %f y: %f\n", u_vector[j], y.value);
-        WindowFilterStoreStateVariable(&y);
+        // WindowFilterStoreStateVariable(&y);
     }
 
     return 0;
--- window_filter/window_filter.c
+++ window_filter/window_filter.c
@@ -8,26 +8,26 @@ void WindowFilterInit(window_filter_t * const instance, const float init_value) 
 
 // define window filter function
 void WindowFilterFunc(window_filter_t * const instance, const float input_value, const float window_size) {
-    if (
-        ( (input_value - instance->value) > window_size ) ||
-        ( (input_value - instance->value) < -window_size )
-        )
-
-        instance->value = instance->value_z;
-    else
-        instance->value = input_value;
-    
     // if (
-    //     !(
     //     ( (input_value - instance->value) > window_size ) ||
     //     ( (input_value - instance->value) < -window_size )
     //     )
-    // )
+
+    //     instance->value = instance->value_z;
+    // else
     //     instance->value = input_value;
+    
+    if (
+        !(
+        ( (input_value - instance->value) > window_size ) ||
+        ( (input_value - instance->value) < -window_size )
+        )
+    )
+        instance->value = input_value;
 
 }
 
-// define function to store state variable
-void WindowFilterStoreStateVariable(window_filter_t * const instance) {
-    instance->value_z = instance->value;
-}
+// // define function to store state variable
+// void WindowFilterStoreStateVariable(window_filter_t * const instance) {
+//     instance->value_z = instance->value;
+// }
