954b76b62536055c5b329c521c8ffa96ac4fd62a
Rahul <rahul@treau.cool>
2021-06-20 16:11:40 -0700

    1. Added store state variable function

--- window_filter/unit_test.c
+++ window_filter/unit_test.c
@@ -3,13 +3,14 @@
 
 int main() {
 
-    float u_vector[] = {1.0, 1.1, 0.9, 1.2, 3.1, 1.2, 3.3, 1.0, 1.1, 0.9};
+    float u_vector[] = {1.0, 1.1, 0.9, 1.2, 3.1, 1.2, 3.3, 3.5, 1.1, 0.9, 1.0, 1.1, 0.9, 1.2, 3.1, 1.2, 3.3, 3.5, 1.1, 0.9};
     window_filter_t y;
     WindowFilterInit(&y, u_vector[0]);
 
     for (int j = 0; j < sizeof(u_vector) / sizeof(u_vector[0]); j ++) {
         WindowFilterFunc(&y, u_vector[j], 2.0);
         printf("u: %f y: %f\n", u_vector[j], y.value);
+        WindowFilterStoreStateVariable(&y);
     }
 
     return 0;
--- window_filter/window_filter.c
+++ window_filter/window_filter.c
@@ -1,12 +1,12 @@
 #include "window_filter.h"
 
-// initialization function
+// define initialization function
 void WindowFilterInit(window_filter_t * const instance, const float init_value) {
     instance->value = init_value;
     instance->value_z = init_value;
 }
 
-// window filter function
+// define window filter function
 void WindowFilterFunc(window_filter_t * const instance, const float input_value, const float window_size) {
     if (
         ( (input_value - instance->value) > window_size ) ||
@@ -17,5 +17,17 @@ void WindowFilterFunc(window_filter_t * const instance, const float input_value,
     else
         instance->value = input_value;
     
+    // if (
+    //     !(
+    //     ( (input_value - instance->value) > window_size ) ||
+    //     ( (input_value - instance->value) < -window_size )
+    //     )
+    // )
+    //     instance->value = input_value;
+
+}
+
+// define function to store state variable
+void WindowFilterStoreStateVariable(window_filter_t * const instance) {
     instance->value_z = instance->value;
 }
