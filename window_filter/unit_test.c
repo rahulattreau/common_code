dbdd4dc1193792ddb35748c0a37187097494903e
Rahul <rahul@treau.cool>
2021-06-20 17:58:06 -0700

    1. merged in changes from window_filter_2 2. Removed the window_filter_t data type, as window_filter_t was getting subtracted from float. Even though they are the same, as practice I wanted them both to have the same expiclitly typed data types

--- window_filter/unit_test.c
+++ window_filter/unit_test.c
@@ -3,6 +3,7 @@
 
 int main() {
 
+    float_state_and_state_z u;
     float u_vector[] = {1.0, 1.1, 0.9, 1.2, 
     3.1, // big deviation but still within range
     1.2, 1.3, 
@@ -12,14 +13,21 @@ int main() {
     1.2, 
     3.3, 3.5, // out of range
     1.1, 0.9};
+    
+    u.value = u_vector[0];
+    u.value_z = u_vector[0];
 
-    window_filter_t y;
-    WindowFilterInit(&y, u_vector[0]);
+    float y;
+
+    WindowFilterInit(&y, u.value);
 
     for (int j = 0; j < sizeof(u_vector) / sizeof(u_vector[0]); j ++) {
-        WindowFilterFunc(&y, u_vector[j], 2.0);
-        printf("u: %f y: %f\n", u_vector[j], y.value);
-        // WindowFilterStoreStateVariable(&y);
+        u.value = u_vector[j];
+
+        WindowFilterFunc(&y, &u, 2.0);
+        printf("u: %f y: %f\n", u.value, y);
+
+        u.value_z = u.value;
     }
 
     return 0;
--- window_filter/window_filter.h
+++ window_filter/window_filter.h
@@ -1,21 +1,14 @@
 /*
 Description:
-1. If absolute difference in value from current time step to previous time step 
-is greater than window size, retain value.
-2. Else, use new value
+1. If absolute difference in value from current time step to previous time step of input 
+is greater than window size, retain window state variable value.
+2. Else, use new input value
 */
 
-// type definition
-typedef struct {
-    float value; // state variable
-    float value_z; // state variable from previous time step
-} window_filter_t;
+#include "../data_type_state_variable/float_state_and_state_z.h"
 
 // declare initialization function
-void WindowFilterInit(window_filter_t * const instance, const float init_value);
+void WindowFilterInit(float * const state_variable, const float init_value);
 
 // declare window filter function
-void WindowFilterFunc(window_filter_t * const instance, const float input_value, const float window_size);
-
-// declare store state variable
-void WindowFilterStoreStateVariable(window_filter_t * const instance);
+void WindowFilterFunc(float * const instance, float_state_and_state_z * const input_value, const float window_size);
