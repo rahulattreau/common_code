bdb585173c9448e0292a0ebd9b116a2fd3bfbc91
Rahul <rahul@treau.cool>
2021-06-25 19:38:23 -0700

    This looks complete but too verbose.

--- pid_controller/pid_controller.c
+++ pid_controller/pid_controller.c
@@ -69,14 +69,15 @@ i_out
 */
 
 // calculate integral output
-i_out_bus_t IntegralFunction(
+void IntegralFunction(
+    i_out_bus_t * const i_out_bus,
     const float i_gain,
     const float up_sat_value,
     const float lo_sat_value,
     const float init_value,
     const float time_step,
     const float error, 
-    const float reset, 
+    const bool reset, 
     float * const i_out_z,
     float * const pre_sat_value
     ) {
@@ -86,14 +87,17 @@ i_out_bus_t IntegralFunction(
     float pre_sat_dead_zone = 0.0;
     float signum_dead_zone_out = 0.0;
     float i_out = 0.0;
-    i_out_bus_t i_out_bus;
+    bool clamping_condition = false;
     
     integrand = i_gain * error;
-    signum_integrand = SignumFunction(integrand);
-    pre_sat_dead_zone = DeadZone(*pre_sat_value, up_sat_value, lo_sat_value);
-    signum_dead_zone_out = SignumFunction(pre_sat_dead_zone);
-
-    if ( (signum_integrand == signum_dead_zone_out) && (pre_sat_dead_zone != 0) )
+    
+    clamping_condition = IntegralClamping(
+    integrand, 
+    *pre_sat_value, 
+    up_sat_value, 
+    lo_sat_value );
+    
+    if (clamping_condition)
         integrand = 0.0;
 
     if(reset) {
@@ -105,15 +109,31 @@ i_out_bus_t IntegralFunction(
     }
 
     // assign values to i_out_bus
-    i_out_bus = (i_out_bus_t){
+    *i_out_bus = (i_out_bus_t){
         .integrand = integrand,
         .signum_integrand = signum_integrand,
         .pre_sat_dead_zone = pre_sat_dead_zone,
         .signum_dead_zone_out = signum_dead_zone_out,
+        .clamping_condition = clamping_condition,
         .i_out = i_out
     };
+}
 
-    return i_out_bus;
+bool IntegralClamping(
+    const float integrand, 
+    const float pre_sat_value, 
+    const float up_sat_value, 
+    const float lo_sat_value ) {
+
+    bool clamping_condition = false;
+    const int8_t signum_integrand = SignumFunction(integrand);
+    const float pre_sat_dead_zone = DeadZone(pre_sat_value, up_sat_value, lo_sat_value);
+    const int8_t signum_dead_zone_out = SignumFunction(pre_sat_dead_zone);
+
+    if ( (signum_integrand == signum_dead_zone_out) && (pre_sat_dead_zone != 0) )
+        clamping_condition = true;
+    
+    return clamping_condition;
 }
 
 /*
@@ -136,6 +156,7 @@ d_argument_filtered_z
 */
 
 d_out_bus_t DifferentialFunction(
+    d_out_bus_t * const d_out_bus,
     const float d_gain, 
     const float d_filter_tau, 
     const float time_step, 
@@ -146,8 +167,7 @@ d_out_bus_t DifferentialFunction(
     float d_argument = 0.0;
     float d_out = 0.0;
     float d_argument_filtered = 0.0;
-    d_out_bus_t d_out_bus;
-
+    
     d_argument = -1.0 * d_gain * sensed_value;
     d_argument_filtered = LpfOrder1(d_filter_tau, time_step, controller_reset, d_argument, *d_argument_filtered_z);
 
@@ -159,14 +179,11 @@ d_out_bus_t DifferentialFunction(
         d_out = (d_argument_filtered - *d_argument_filtered_z) / time_step;
     
     // assign values to d_out_bus
-    d_out_bus = (d_out_bus_t) {
+    *d_out_bus = (d_out_bus_t) {
         .d_argument = d_argument,
         .d_out = d_out,
         .d_argument_filtered = d_argument_filtered,
     };
-    
-    return d_out_bus;
-
 }
 
 /*
@@ -187,26 +204,34 @@ post_sat_value
 */
 
 // calculate the summer and saturation
-// float SumAndSat(const float p_out, const float i_out, const float d_out, const float up_sat_value, const float lo_sat_value, const float bc_gain) {
-//     float pre_sat_value = 0.0;
-//     float post_sat_value = 0.0;
-//     static float bc_out = 0.0;
+void SumAndSat(
+    float * post_sat_value,
+    const float p_out, 
+    const i_out_bus_t i_out_bus, 
+    const d_out_bus_t d_out_bus, 
+    const float up_sat_value, 
+    const float lo_sat_value, 
+    const float bc_gain
+    ) {
+    float pre_sat_value = 0.0;
+    // float post_sat_value = 0.0;
+    static float bc_out = 0.0;
 
-//     // calculate pre-saturated value
-//     pre_sat_value = p_out + i_out + d_out;
+    // calculate pre-saturated value
+    pre_sat_value = p_out + i_out_bus.i_out + d_out_bus.d_out;
 
-//     // calculate post-saturated value
-//     if (pre_sat_value > up_sat_value)
-//         post_sat_value = up_sat_value;
-//     else if(pre_sat_value < lo_sat_value)
-//         post_sat_value = lo_sat_value;
-//     else
-//         post_sat_value = pre_sat_value;
+    // calculate post-saturated value
+    if (pre_sat_value > up_sat_value)
+        *post_sat_value = up_sat_value;
+    else if(pre_sat_value < lo_sat_value)
+        *post_sat_value = lo_sat_value;
+    else
+        *post_sat_value = pre_sat_value;
     
-//     // calculate back-calculation output for using in next time step
-//     bc_out = bc_gain * (post_sat_value - pre_sat_value);
+    // calculate back-calculation output for using in next time step
+    bc_out = bc_gain * (*post_sat_value - pre_sat_value);
 
-// }
+}
 
 // define initialize pid control
 void PidControl_Constructor(
@@ -226,7 +251,56 @@ void PidControl_Constructor(
 
 }
 
-// // define pid function
-// void PidControl_Step(input_bus_t * const input_bus, output_bus_t * const output_bus) {
+// define pid function
+void PidControl_Step(parameters_t * const parameters, input_bus_t * const input_bus, output_bus_t * const output_bus) {
 
-// }
+    // error function -> deadzone 
+    // -> proportional function, integral function,differential function
+    // -> sum and saturate 
+
+    // error function
+    ErrorFunction( &(output_bus->error), input_bus );
+
+    // deadzone
+    output_bus->error = DeadZone( output_bus->error, parameters->up_sat_value, parameters->lo_sat_value );
+    
+    // proportional function
+    ProportionalFunction( &(output_bus->p_out), parameters->p_gain, output_bus->error );
+    
+    // integral function
+    IntegralFunction(
+    &(output_bus->i_out_bus),
+    parameters->i_gain,
+    parameters->up_sat_value,
+    parameters->lo_sat_value,
+    parameters->init_value,
+    parameters->time_step,
+    output_bus->error, 
+    input_bus->reset, 
+    &(output_bus->i_out_bus.i_out),
+    &(output_bus->sat_and_sum_out_bus.pre_sat_value)
+    );
+    
+    // differential function
+    DifferentialFunction(
+        &(output_bus->d_out_bus),
+        parameters->d_gain, 
+        parameters->d_filter_tau, 
+        parameters->time_step, 
+        input_bus->reset, 
+        input_bus->sensed_value,
+        &(output_bus->d_out_bus.d_argument_filtered)
+        );
+    
+    // sum and saturate
+    SumAndSat(
+        &(output_bus->sat_and_sum_out_bus.post_sat_value),
+        output_bus->p_out, 
+        output_bus->i_out_bus, 
+        output_bus->d_out_bus, 
+        parameters->up_sat_value, 
+        parameters->lo_sat_value, 
+        parameters->bc_gain
+    ); 
+
+    }
--- pid_controller/pid_controller.h
+++ pid_controller/pid_controller.h
@@ -1,11 +1,16 @@
 #include <stdbool.h>
 
+#ifdef __INT8_TYPE__
+typedef __INT8_TYPE__ int8_t;
+#endif
+
 // define integral output data type:
 typedef struct {
     float integrand;
     float signum_integrand;
     float pre_sat_dead_zone;
     float signum_dead_zone_out;
+    bool clamping_condition;
     float i_out;
 } i_out_bus_t;
 
@@ -55,7 +60,7 @@ typedef struct {
     sat_and_sum_bus_t sat_and_sum_out_bus;
 } output_bus_t;
 
-// ===== define member functions =====
+// ===== declare public member functions =====
 
 void PidControl_Constructor(
     input_bus_t *_input_bus,
@@ -64,16 +69,41 @@ void PidControl_Constructor(
     bool *reset_pointer
     );
 
+// ===== declare private member functions =====
+
+// error function
 void ErrorFunction(float * const error, input_bus_t * const input_bus);
+
+// proportional function
 void ProportionalFunction(float * const p_out, const float p_gain, const float error);
-i_out_bus_t IntegralFunction(
+
+// integral function
+void IntegralFunction(
+    i_out_bus_t * const i_out_bus,
     const float i_gain,
     const float up_sat_value,
     const float lo_sat_value,
     const float init_value,
     const float time_step,
     const float error, 
-    const float reset, 
+    const bool reset, 
     float * const i_out_z,
     float * const pre_sat_value
     );
+
+// integral clamping
+bool IntegralClamping(
+    const float integrand, 
+    const float pre_sat_value, 
+    const float up_sat_value, 
+    const float lo_sat_value );
+
+// differential function
+d_out_bus_t DifferentialFunction(
+    d_out_bus_t * const d_out_bus,
+    const float d_gain, 
+    const float d_filter_tau, 
+    const float time_step, 
+    bool controller_reset, 
+    const float sensed_value,
+    float * const d_argument_filtered_z);
