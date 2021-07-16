a3dda9b2aa89414f25af252a158d6273473315d3
Rahul <rahul@treau.cool>
2021-07-15 23:30:32 -0700

    fixed the first time step reset

--- accumulator/accumulator.c
+++ accumulator/accumulator.c
@@ -10,16 +10,18 @@ void Accumulator_Constructor(accumulator_t * const instance) {
 void Accumulator_Step(accumulator_t * const instance, const float xk, const float init_val, const bool reset) {
     
     float input = 0.0;
+    bool reset_accumulator = !(instance->yk_1_.init_) || reset; // this ensures reset in first time step
 
     // execute unit delay step
     UnitDelay_Step( &(instance->yk_1_), init_val, reset );
 
     // execute accumulator function
-    if (!reset)
+    if (!reset_accumulator)
         input = xk;
         
     instance->yk_ = input + instance->yk_1_.yk_;
     
     // execute unit delay post step
     UnitDelay_PostStep( &(instance->yk_1_), instance->yk_ );
+
 }
