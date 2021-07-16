775b8a562812fde3caa9532c891158bd27d128ac
Rahul <rahul@treau.cool>
2021-07-16 16:23:35 -0700

    added comment

--- saturator/saturator.h
+++ saturator/saturator.h
@@ -1,1 +1,7 @@
+/* 
+description:
+1. this is a saturator, it returns the same value in the range, a limited value outside the range
+2. limit values are part of the acceptable range, i.e. range = [lower limit, upper limit]
+*/
+
 float Saturator( const float u, float upper_limit, float lower_limit );
